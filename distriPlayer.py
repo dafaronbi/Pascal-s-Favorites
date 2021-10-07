ab4fe9fdc175bcd8708118973a358f1c0dcf3313
dafaronbi <dprdctions@gmail.com>
2021-10-07 13:38:29 -0400

    working plot switcher

--- distriPlayer.py
+++ distriPlayer.py
@@ -1,12 +1,11 @@
+#import needed libraries
 import tkinter as tk
 import numpy as np
-
 from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, NavigationToolbar2Tk)
-
-# Implement the default Matplotlib key bindings.
-from matplotlib.backend_bases import key_press_handler
 from matplotlib.figure import Figure
 
+t = np.arange(0, 3, .01)
+distributions = {"normal": 2 * np.sin(2 * np.pi * t), "t": 2 * np.sin(4 * np.pi * t), "beta": 2 * np.sin(50 * np.pi * t)}
 
 class d_player(tk.Frame):
     def __init__(self, master=None):
@@ -26,51 +25,51 @@ class d_player(tk.Frame):
         self.mainView = tk.Frame(self)
         self.mainView.pack(side="left")
 
-        #add buttons to sidbar
-        self.normal = tk.Button(self.sideBar)
-        self.t = tk.Button(self.sideBar)
-        self.binomial = tk.Button(self.sideBar)
-
         #configure buttons
-        self.normal["text"] = "Normal Distribution"
-        self.t["text"] = "t Distribution"
-        self.binomial["text"] = "binomial Distribution"
+        for plot in distributions:
+            #add to sidebar
+            button = tk.Button(self.sideBar)
 
-        self.normal["command"] = lambda: self.create_plot("normal")
-        self.t["command"] = lambda: self.create_plot("t")
-        self.binomial["command"] = lambda: self.create_plot("binomial")
+            #add button text as distribution name
+            button["text"] = plot
 
-        #place buttons
-        self.normal.pack(side="top")
-        self.t.pack(side="top")
-        self.binomial.pack(side="top")
+            #add button command to switch the plot
+            button["command"] = lambda plot=plot: self.switch_plot(plot)
 
-        #add canvas for mat plot
-        self.draw_plot(self.mainView)
+            #pack buttons
+            button.pack(side="top")
 
-        self.quit = tk.Button(self, text="QUIT", fg="red",
-                              command=self.master.destroy)
+        #configure frames which hold plots
+        self.frames = {}
+        for plot in distributions:
+
+            frame = tk.Frame(self.mainView)
+            # add canvas for mat plot
+            fig = Figure(figsize=(5, 4), dpi=100)
+            y = distributions[plot]
+            fig.add_subplot(111).plot(y)
+
+            canvas = FigureCanvasTkAgg(fig, master=frame)  # A tk.DrawingArea.
+            canvas.draw()
+            canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+
+            self.frames[plot] = frame
+            frame.grid(row=0, column=0, sticky="nsew")
+
+        self.switch_plot("normal")
+
+        self.quit = tk.Button(self, text="QUIT", fg="red", command=self.master.destroy)
         self.quit.pack(side="bottom")
 
-    def create_plot(self,type2):
-        self.type = type2
-        self.draw_plot(self.mainView)
+    def switch_plot(self,plot):
+        frame = self.frames[plot]
+        frame.tkraise()
 
-    def draw_plot(self, parent):
-        t = np.arange(0, 3, .01)
-        get_plot = {"normal": 2 * np.sin(2 * np.pi * t), "t": 2 * np.sin(4 * np.pi * t), "binomial":2 * np.sin(50 * np.pi * t)}
+def main():
+    root = tk.Tk()
+    root.wm_title("Distribution Player")
+    player = d_player(master=root)
+    player.mainloop()
 
-        # add canvas for mat plot
-        fig = Figure(figsize=(5, 4), dpi=100)
-        y = get_plot[self.type]
-        fig.add_subplot(111).plot(t, y)
-
-        self.canvas = FigureCanvasTkAgg(fig, master=parent)  # A tk.DrawingArea.
-        self.canvas.draw()
-        self.canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
-
-
-root = tk.Tk()
-root.wm_title("Distribution Player")
-player = d_player(master=root)
-player.mainloop()
+if __name__ == "__main__":
+    main()
